48a6f4c55c28f6d5ab6cdb0fdd6df2d9e3d8801b
Tim Griffith <grifmang@gmail.com>
2020-04-24 13:06:10 -0400

    ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,32 +1,65 @@
-from doubly_linked_list import DoublyLinkedList
+
+class Node:
+    def __init__(self, value=None, next_node=None):
+        # the value at this linked list node
+        self.value = value
+        # reference to the next node in the list
+        self.next_node = next_node
+
+    def get_value(self):
+        return self.value
+
+    def get_next(self):
+        return self.next_node
+
+    def set_next(self, new_next):
+        # set this node's next_node reference to the passed in node
+        self.next_node = new_next
 
 
-class RingBuffer:
-    def __init__(self, capacity):
-        self.capacity = capacity
-        self.current = None
-        self.storage = DoublyLinkedList()
-
-    def append(self, item):
-        pass
-
-    def get(self):
-        # Note:  This is the only [] allowed
-        list_buffer_contents = []
-
-        # TODO: Your code here
-
-        return list_buffer_contents
-
-# ----------------Stretch Goal-------------------
+class LinkedList():
+    def __init__(self):
+        # reference to the head of the list
+        self.head = None
 
 
-class ArrayRingBuffer:
-    def __init__(self, capacity):
-        pass
+    def add_to_head(self, value):
+        node = Node(value)
+        if self.head is not None:
+            node.set_next(self.head)
 
-    def append(self, item):
-        pass
+        self.head = node
 
-    def get(self):
-        pass
+    def contains(self, value):
+        if not self.head:
+            return False
+        # get a reference to the node we're currently at; update this as we
+        # traverse the list
+        current = self.head
+        # check to see if we're at a valid node
+        while current:
+            # return True if the current value we're looking at matches our
+            # target value
+            if current.get_value() == value:
+                return True
+            # update our current node to the current node's next node
+            current = current.get_next()
+        # if we've gotten here, then the target node isn't in our list
+        return False
+
+    
+
+    def reverse_list(self, node, prev):
+        # You must use recursion for this solution
+        # print(node.value)
+        # print(prev)
+        if node == None:
+            return
+        if node.next_node == None:
+            node.next_node = prev
+            self.head = node
+            return
+        else:
+            next = node.next_node
+            node.next_node = prev
+            self.reverse_list(next, node)
